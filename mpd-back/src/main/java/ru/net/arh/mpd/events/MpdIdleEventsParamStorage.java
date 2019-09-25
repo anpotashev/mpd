5e5c262418ffd04fdea0a897fffa21355faa5061
Aleksandr N Potashev <anpotashev@hotmail.com>
2019-09-25 22:21:40 +0300

    bugfix NPE

--- mpd-back/src/main/java/ru/net/arh/mpd/events/MpdIdleEventsParamStorage.java
+++ mpd-back/src/main/java/ru/net/arh/mpd/events/MpdIdleEventsParamStorage.java
@@ -7,17 +7,20 @@ import ru.net.arh.mpd.model.events.MpdEventType;
 import java.lang.reflect.Method;
 import java.util.*;
 
+import static java.util.Collections.EMPTY_LIST;
+import static java.util.Collections.EMPTY_SET;
+
 public class MpdIdleEventsParamStorage {
 
     private static Map<MpdIdleType, List<IdleMethod>> map = new HashMap<>();
     private static Map<MpdIdleType, Set<String>> cacheMap = new HashMap<>();
 
     static Collection<String> cacheNames(MpdIdleType type) {
-        return cacheMap.get(type);
+        return cacheMap.getOrDefault(type, EMPTY_SET);
     }
 
     static Collection<IdleMethod> idleMethods(MpdIdleType type) {
-        return map.get(type);
+        return map.getOrDefault(type, EMPTY_LIST);
     }
 
     static void add(Object bean, MpdEventType type, Method method, MpdIdleType[] types, String[] cacheNames) {
